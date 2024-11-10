dfc67e623813bbfbd1613f16e066dc39113894c3
mmhussein13 <mmhussein13@gmail.com>
2024-11-10 20:31:44 +0200

    10-11-2024

--- orders/admin.py
+++ orders/admin.py
@@ -2,6 +2,36 @@ from django.contrib import admin
 from .models import Payment, Order, OrderProduct
 
 # Register your models here.
-admin.site.register(Payment)
-admin.site.register(Order)
+class OrderProductInline(admin.TabularInline):
+    model = OrderProduct
+    readonly_fields = ('payment', 'user', 'product', 'quantity', 'product_price', 'ordered')
+    extra = 0
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        'order_number',
+        'full_name',
+        'phone_number',
+        'email',
+        'city',
+        'order_total',
+        'tax',
+        'status',
+        'is_ordered',
+        'created_at',
+    ]
+    list_filter = ['status', 'is_ordered']
+    search_fields = ['order_number', 'first_name', 'last_name', 'phone_number', 'email']
+    list_per_page = 20
+    inlines = [OrderProductInline]
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ['user', 'payment_id', 'payment_method', 'amount_paid', 'status', 'created_at']
+    search_fields = ['payment_id', 'amount_paid', 'status', 'created_at']
+    list_per_page = 20
+
+
+admin.site.register(Payment, PaymentAdmin)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderProduct)
--- orders/models.py
+++ orders/models.py
@@ -12,7 +12,7 @@ class Payment(models.Model):
     created_at      = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.payment
+        return self.payment_id
 
 
 class Order(models.Model):
@@ -44,8 +44,14 @@ class Order(models.Model):
     created_at      = models.DateTimeField(auto_now_add=True)
     update_at       = models.DateTimeField(auto_now=True)
 
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def full_address(self):
+        return f'{self.address_line_1} {self.address_line_2}'
+
     def __str__(self):
-        return self.user.first_name
+        return str(self.user.first_name)
 
 
 class OrderProduct(models.Model):
@@ -53,9 +59,7 @@ class OrderProduct(models.Model):
     payment = models.ForeignKey(Payment, on_delete=models.SET_NULL, blank=True, null=True)
     user = models.ForeignKey(Account, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    variation = models.ForeignKey(Variation, on_delete=models.CASCADE)
-    color = models.CharField(max_length=50)
-    size = models.CharField(max_length=50)
+    variations  = models.ManyToManyField(Variation, blank=True)
     quantity = models.IntegerField()
     product_price = models.FloatField()
     ordered = models.BooleanField(default=False)
